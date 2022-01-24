04388576411ad9a188105d61f26338d3f8a19756
hamelsmu <hamel.husain@gmail.com>
2022-01-24 15:54:11 -0800

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_long_description() -> str:
 
 setup(
     name="metaflow-card-html",
-    version="1.0.0",
+    version="1.0.1",
     description="A metaflow card that renders HTML inputs.",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",

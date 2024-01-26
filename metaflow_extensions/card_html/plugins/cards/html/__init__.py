0bea7edc530bc22728c65a97232ad4efcd15fe4f
Ville Tuulos <tuulos@gmail.com>
2024-01-25 19:56:15 -0800

    allow html to be specified with refresh()

--- metaflow_extensions/card_html/plugins/cards/html/__init__.py
+++ metaflow_extensions/card_html/plugins/cards/html/__init__.py
@@ -1,14 +1,23 @@
 from metaflow.cards import MetaflowCard
 
+DEFAULT = "<html><body>no html specified</body></html>"
+
 class HTMLCard(MetaflowCard):
 
+    ALLOW_USER_COMPONENTS = True
+    RUNTIME_UPDATABLE = True
     type = 'html'
-    
+
     def __init__(self, options={"attribute":"html"}, **kwargs):
         self._attr_nm = options.get("attribute", "html")
- 
+
     def render(self, task):
+        rt_data = getattr(self, 'runtime_data', {}).get("user")
         if self._attr_nm in task:
             return str(task[self._attr_nm].data)
+        elif rt_data:
+            return rt_data['html']
+        else:
+            return DEFAULT
 
 CARDS = [HTMLCard]
